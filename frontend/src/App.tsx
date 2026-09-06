296e820aca7ea95a351df8e10e9a58b1b842c9e2
Kleberson Duarte <klebersonduartesantos39@gmail.com>
2026-09-06 18:33:17 -0300

    chore:Utilizando o React Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
-//import Login from "./apps/login/login";
-//import Dashboard from "./apps/dashboard/dashboard";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Login from "./apps/login/login";
+import Dashboard from "./apps/dashboard/dashboard";
 import Register from "./apps/register/register";
 
 function App() {
   return (
     <>
-      {/* <Login /> */}
-      {/*<Dashboard />*/}
-      <Register />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
