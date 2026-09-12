29d2250e9979d90da0b50137d2b5e297ec9feb7a
Kleberson Duarte <klebersonduartesantos39@gmail.com>
2026-09-12 16:20:52 -0300

    feat:adicionando lógica de login do usuário

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -16,3 +16,23 @@ export const postUser = (req, res) => {
        return res.status(200).json({success: true, message: "User registered successfully",userId: data.insertId}); 
     });
 }
+
+export const getUser = (req, res) => {
+    const { email, password } = req.body;
+    const salt = bcrypt.genSaltSync(10);
+
+    const sql = "SELECT * FROM users WHERE email_usuario = ? && senha_usuario = ?";
+
+    db.query(sql, [email, salt], (err, data) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+
+        if (data.length === 0) {
+            return res.status(401).json({ error: "Invalid email or password" });
+        }
+
+        return res.status(200).json({ success: true, message: "User logged in successfully", userId: data[0].id });
+
+    });
+}
