68b5368f3366ffc194f1e2b947f32d55641b0689
Kleberson Duarte <klebersonduartesantos39@gmail.com>
2026-09-12 16:09:21 -0300

    feat:adicionando cryptografica de senha

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,11 +1,13 @@
 import {db} from "../db.js";
+import bcrypt from "bcryptjs";
 
 export const postUser = (req, res) => {
     const { name, email, password } = req.body;
+    const salt = bcrypt.genSaltSync(10);
     const sql = "INSERT INTO users (nome_usuario, email_usuario, senha_usuario) VALUES (?, ?, ?)";
 
 
-    db.query(sql, [name, email, password], (err, data) => {
+    db.query(sql, [name, email, salt], (err, data) => {
         if (err) {
             return res.status(500).json({ error: err.message });
         }
