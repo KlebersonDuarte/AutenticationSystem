714698fb104123f32390f2256cedbd361e70b507
Kleberson Duarte <klebersonduartesantos39@gmail.com>
2026-09-12 18:09:07 -0300

    feat:adicionando login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,38 +1,55 @@
-import {db} from "../db.js";
-import bcrypt from "bcryptjs";
+import { db } from "../db.js";
+import bcrypt from "bcrypt";
 
-export const postUser = (req, res) => {
-    const { name, email, password } = req.body;
-    const salt = bcrypt.genSaltSync(10);
-    const sql = "INSERT INTO users (nome_usuario, email_usuario, senha_usuario) VALUES (?, ?, ?)";
+export const postUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const hash = await bcrypt.hash(password, 10);
+        const sql = "INSERT INTO users (nome_usuario, email_usuario, senha_usuario) VALUES (?, ?, ?)";
 
 
-    db.query(sql, [name, email, salt], (err, data) => {
-        if (err) {
-            return res.status(500).json({ error: err.message });
-        }
+        db.query(sql, [name, email, hash], (err, data) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
 
-        console.log("User registered successfully");
-       return res.status(200).json({success: true, message: "User registered successfully",userId: data.insertId}); 
-    });
-}
+            console.log("User registered successfully");
+            return res.status(200).json({ success: true, message: "User registered successfully", userId: data.insertId });
+        });
+    } catch (error) {
+        return res.status(500).json({error: error.message});
+    }}
 
-export const getUser = (req, res) => {
-    const { email, password } = req.body;
-    const salt = bcrypt.genSaltSync(10);
+    export const getUser = async (req, res) => {
+        try{
+        const { email, password } = req.body;
 
-    const sql = "SELECT * FROM users WHERE email_usuario = ? && senha_usuario = ?";
+        const sql = "SELECT * FROM users WHERE email_usuario = ?";
 
-    db.query(sql, [email, salt], (err, data) => {
-        if (err) {
-            return res.status(500).json({ error: err.message });
-        }
+        db.query(sql, [email], async (err, data) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
 
-        if (data.length === 0) {
-            return res.status(401).json({ error: "Invalid email or password" });
-        }
+            if (data.length === 0) {
+                return res.status(401).json({ error: "Invalid email or password" });
+            }
 
-        return res.status(200).json({ success: true, message: "User logged in successfully", userId: data[0].id });
+            const user = data[0];
 
-    });
-}
+            const passwordCorrect = await bcrypt.compare(
+                password,
+                user.senha_usuario
+            );
+
+            if (!passwordCorrect) {
+                return res.status(401).json({
+                    error: "Invalid email or password"
+                });
+            }
+
+            return res.status(200).json({ success: true, message: "User logged in successfully", userId: user.id });
+
+        });}catch (error) {
+        return res.status(500).json({error: error.message});}
+    }
